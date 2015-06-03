6e664ce5ec6ee4a88a60af273dded07b6999d308
ErichMeisner <em031@hdm-stuttgart.de>
2015-06-03 15:32:02 +0200

    Klasse LoginInfo angelegt.
    Klasse HDM-SMS teilweise für Login angepasst (Attribute)

---
+++ HdM SMS/src/de/hdm/sms/shared/LoginInfo.java
@@ -0,0 +1,60 @@
+package de.hdm.sms.shared;
+
+import de.hdm.sms.shared.bo.User;
+import java.io.Serializable;
+
+/**
+ * Diese Klasse enthält alle Daten über die aktuelle Nutzersession
+ * 
+ * @author Fabian
+ *
+ */
+public class LoginInfo implements Serializable {
+
+	private static final long serialVersionUID = -5207880593956618550L;
+	private boolean loggedIn = false;
+	private String loginUrl;
+	private String logoutUrl;
+	private String emailAddress;
+	private User user;
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+
+	public String getLoginUrl() {
+		return loginUrl;
+	}
+
+	public void setLoginUrl(String loginUrl) {
+		this.loginUrl = loginUrl;
+	}
+
+	public String getLogoutUrl() {
+		return logoutUrl;
+	}
+
+	public void setLogoutUrl(String logoutUrl) {
+		this.logoutUrl = logoutUrl;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
