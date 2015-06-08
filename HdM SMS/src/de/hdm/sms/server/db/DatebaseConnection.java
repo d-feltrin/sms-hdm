1e271a58910cde29883f9a4053f4cde0da2b397b
Denis Feltrin <d.feltrin@icloud.com>
2015-06-08 17:19:02 +0200

    #neue dbcon

--- HdM SMS/src/de/hdm/sms/server/db/DatebaseConnection.java
+++ HdM SMS/src/de/hdm/sms/server/db/DatebaseConnection.java
@@ -3,33 +3,38 @@ package de.hdm.sms.server.db;
 import com.google.appengine.api.rdbms.AppEngineDriver;
 import java.sql.*;
 
-
 public class DatebaseConnection {
- 		  
+
 	private static Connection con = null;
 
 	public static Connection connection() {
-		  
-		/**Falls die DB-Connection noch nicht besteht, führe nachfolgende Befehle aus.*/
+
+		/**
+		 * Falls die DB-Connection noch nicht besteht, führe nachfolgende
+		 * Befehle aus.
+		 */
 		if (con == null) {
-				 
+
 			try {
 				DriverManager.registerDriver(new AppEngineDriver());
-		    
 
-con = DriverManager.getConnection("jdbc:mysql://85.214.149.12:3306/db_sms", "smshdm", "hdmitpj");
+				con = DriverManager.getConnection(
+						"jdbc:mysql://85.214.149.12:3306/db_sms", "smshdm",
+						"hdmitpj");
 
-//				con = DriverManager.getConnection("jdbc:google:rdbms://hdm1337:hdmsql/hotelverwaltung", "root", ""); // Create connection with user-credentials
-		   } 
-		   
-		   catch (SQLException e1) {
-		    con = null;
-		   
-		    e1.printStackTrace();
-		   }
-		  }
-		  
-		  return con;
-		 }
+				// con =
+				// DriverManager.getConnection("jdbc:google:rdbms://hdm1337:hdmsql/hotelverwaltung",
+				// "root", ""); // Create connection with user-credentials
+			}
+
+			catch (SQLException e1) {
+				con = null;
+
+				e1.printStackTrace();
+			}
+		}
+
+		return con;
+	}
 
 }
