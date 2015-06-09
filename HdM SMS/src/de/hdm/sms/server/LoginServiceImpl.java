47add7e529e724458e84acde06d301a7576ae333
ErichMeisner <em031@hdm-stuttgart.de>
2015-06-09 18:02:33 +0200

    Fehler behoben

---
+++ HdM SMS/src/de/hdm/sms/server/LoginServiceImpl.java
@@ -0,0 +1,59 @@
+package de.hdm.sms.server;
+
+import de.hdm.sms.server.db.UserMapper;
+import de.hdm.sms.shared.LoginInfo;
+import de.hdm.sms.shared.LoginService;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * Meldet den User über den Google Accounts Service an.
+ * 
+ */
+public class LoginServiceImpl extends RemoteServiceServlet implements
+		LoginService {
+
+	private static final long serialVersionUID = -1682217411238580183L;
+
+	@Override
+	public LoginInfo getUserInfo(String uri) {
+		// Nutzer von Google holen
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		LoginInfo loginInfo = new LoginInfo();
+
+		if (user != null) {
+
+			UserMapper userMapper = UserMapper.userMapper();
+
+			loginInfo.setLoggedIn(true);
+			loginInfo.setEmailAddress(user.getEmail());
+			loginInfo.setLogoutUrl(userService.createLogoutURL(uri));
+
+			// In der Datenbank nachsehen, ob User schon registiert ist.
+			de.hdm.sms.shared.bo.User 
+			User = (userMapper
+					.getUserByEmail(loginInfo.getEmailAddress()));
+			if (User != null) {
+				loginInfo.setUser(User);
+			}
+			// User 'registrieren'
+			else {
+				User = new de.hdm.sms.shared.bo.User();
+				User.seteMailAdress(loginInfo.getEmailAddress());
+				userMapper.insertUser(User);
+				loginInfo.setUser(userMapper.
+						getUserByEmail(loginInfo.getEmailAddress()));
+			}
+
+		} else {
+			loginInfo.setLoggedIn(false);
+			loginInfo.setLoginUrl(userService.createLoginURL(uri));
+		}
+		return loginInfo;
+
+	}
+}
