3f3dfa8d2b01ad8522b5db47796614c6695564a1
Denis Feltrin <d.feltrin@icloud.com>
2015-06-24 18:55:18 +0200

    ComponentGroup BO angepasst

--- HdM SMS/src/de/hdm/sms/shared/bo/ComponentGroup.java
+++ HdM SMS/src/de/hdm/sms/shared/bo/ComponentGroup.java
@@ -7,6 +7,11 @@ public class ComponentGroup extends BusinessObject {
 	
 	private static final long serialVersionUID = 1L;
 	private String name;
+	private int componentId;
+	private int componentId2;
+	private String lastEditor;
+	private String tag;
+	private int amount;
 	
 public ComponentGroup(){
 		
@@ -23,6 +28,46 @@ public ComponentGroup(){
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getComponentId() {
+		return componentId;
+	}
+
+	public void setComponentId(int componentId) {
+		this.componentId = componentId;
+	}
+
+	public String getLastEditor() {
+		return lastEditor;
+	}
+
+	public void setLastEditor(String lastEditor) {
+		this.lastEditor = lastEditor;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getComponentId2() {
+		return componentId2;
+	}
+
+	public void setComponentId2(int componentId2) {
+		this.componentId2 = componentId2;
 	}
 
 
