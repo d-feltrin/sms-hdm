49c062fbd7f81adf5c7fff784a09e313c7665344
Denis Feltrin <d.feltrin@icloud.com>
2015-07-05 14:16:12 +0200

    Reports erstellt

--- HdM SMS/src/de/hdm/sms/client/report/MaterialResource.java
+++ HdM SMS/src/de/hdm/sms/client/report/MaterialResource.java
@@ -1,46 +1,174 @@
 package de.hdm.sms.client.report;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.sms.shared.AService;
 import de.hdm.sms.shared.AServiceAsync;
+import de.hdm.sms.shared.bo.Component;
+import de.hdm.sms.shared.bo.ComponentGroup;
 import de.hdm.sms.shared.bo.Product;
+import de.hdm.sms.shared.bo.Stocklist;
+import de.hdm.sms.shared.bo.User;
 
 public class MaterialResource extends VerticalPanel {
 	private final AServiceAsync asyncObj = GWT.create(AService.class);
+
+	private ArrayList<Product> allProducts = new ArrayList<Product>();
+
+	private ArrayList<Component> allComponents = new ArrayList<Component>();
+	private ArrayList<Integer> amountListOfComponent = new ArrayList<Integer>();
+
+	private ArrayList<ComponentGroup> allComponentGroups = new ArrayList<ComponentGroup>();
+	private ArrayList<Integer> amountListOfComponentGroup = new ArrayList<Integer>();
+
+	private ArrayList<Component> allComponentsOfProduct = new ArrayList<Component>();
+	private ArrayList<Integer> amountComponentsOfProduct = new ArrayList<Integer>();
+
+	// GUI Objects
+	// Select (FIRSTPAGE)
 	private VerticalPanel PanelToGenerateProductResources = new VerticalPanel();
 	private ListBox ListBoxOfChoseableProducts = new ListBox();
-	private TextBox AmountOfChosenProducts = new TextBox();
-	private Button buttonToGenerateReport = new Button(
-			"Materialbedarf berechnen");
-	
+	private TextBox textBoxAmountOfChosenProducts = new TextBox();
+	private Button buttonToGenerateReport = new Button("Materialbedarf berechnen");
 
-	public MaterialResource() {
+	// Load
+	public void onLoad() {
+		loadAllProducts();
+		loadComponentsANDComponentGroup();
+		// RootPanel Matching
+		RootPanel.get("rightside").clear();
+		PanelToGenerateProductResources.add(new Label("Produkt zur Materialbedarfsrechnung auswählen"));
+		PanelToGenerateProductResources.add(ListBoxOfChoseableProducts);
+		PanelToGenerateProductResources.add(new Label("Anzahl eingeben"));
+		PanelToGenerateProductResources.add(textBoxAmountOfChosenProducts);
+		PanelToGenerateProductResources.add(buttonToGenerateReport);
 
+		RootPanel.get("rightside").add(PanelToGenerateProductResources);
+
+		// Buttonclickhandler
+		buttonToGenerateReport.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				if (ListBoxOfChoseableProducts.getItemText(ListBoxOfChoseableProducts.getSelectedIndex()).equals("---"))
+					Window.alert("Bitte ein gueltiges Produkt auswaehlen");
+				else {
+					// get selected Item
+					int IdOfSelectedItem = getIdFromProduct(ListBoxOfChoseableProducts
+							.getItemText(ListBoxOfChoseableProducts.getSelectedIndex()));
+					for (Product product : allProducts) {
+
+						if (product.getId() == IdOfSelectedItem) {
+							LoadProductlistToGenerateResources(product); // load
+																			// selected
+							// item
+							break;
+						}
+
+					}
+				}
+
+			}
+		});
 	}
 
-	private String getIDbyDropDownText(String selectedProduct) {
+	private void LoadProductlistToGenerateResources(Product product_) {
+		RootPanel.get("rightside").clear();
+		int amountOfProduct = Integer.valueOf(textBoxAmountOfChosenProducts.getText());
 
-		// get ID by ListBox text
-		// Splitt " - 569:Bla" into " - 569", "Bla"
-		String[] SplitStepOne = selectedProduct.split(":");
+		VerticalPanel PanelProductInfo_Name = new VerticalPanel();
+		PanelProductInfo_Name.add(new Label("Produkt:"));
+		PanelProductInfo_Name.add(new Label(product_.getProductName()));
 
-		// Splitt " - 569" into " - ", "569"
-		String[] SplitStepTwo = SplitStepOne[0].split(" ");
+		RootPanel.get("rightside").add(PanelProductInfo_Name);
+		
+		
+		VerticalPanel PanelProductInfo_Anzahl = new VerticalPanel();
+		PanelProductInfo_Anzahl.add(new Label("Anzahl:"));
+		PanelProductInfo_Anzahl.add(new Label(String.valueOf(amountOfProduct)));
 
-		return SplitStepTwo[1];
+		RootPanel.get("rightside").add(PanelProductInfo_Anzahl);
+		
+				
+		ComponentGroup componentGrouToCalcResources = null;
+		for (ComponentGroup componentgroup : allComponentGroups) {
+			if (componentgroup.getId() == product_.getComponentGroupId()) {
+				componentGrouToCalcResources = componentgroup;
+				break;
+			}
+		}
+
+		// Add all ComponentGroups
+		loadAllComponentsOfComponentGroup(componentGrouToCalcResources, amountOfProduct);
+
+		ArrayList<Component> newListOfAggregatedComponents = new ArrayList<Component>();
+		ArrayList<Integer> newListOfAggregatedComponentsAmount = new ArrayList<Integer>();
+
+		for (int i = 0; i < allComponentsOfProduct.size(); i++) {
+			Component component = allComponentsOfProduct.get(i);
+
+			boolean isAlreadyInList = false;
+			int listId = -1;
+			for (int j = 0; j < newListOfAggregatedComponents.size(); j++) {
+				if (newListOfAggregatedComponents.get(j).getId() == component.getId()) {
+					isAlreadyInList = true;
+					listId = j;
+				}
+			}
+
+			if (isAlreadyInList) {
+				int oldAmount = newListOfAggregatedComponentsAmount.get(listId);
+				int newAmount = oldAmount + amountComponentsOfProduct.get(i);
+				newListOfAggregatedComponentsAmount.set(listId, newAmount);
+			} else {
+				newListOfAggregatedComponents.add(component);
+				newListOfAggregatedComponentsAmount.add(amountComponentsOfProduct.get(i));
+			}
+
+		}
+
+		FlexTable flextableComponentsInProduct = new FlexTable();
+		flextableComponentsInProduct.setText(0, 0, "Bauteilname.");
+		flextableComponentsInProduct.setText(0, 1, "Bauteilanzahl");
+
+		for (int j = 0; j < newListOfAggregatedComponents.size(); j++) {
+			int rowNumToInsert = flextableComponentsInProduct.getRowCount();
+			flextableComponentsInProduct.setText(rowNumToInsert, 0, newListOfAggregatedComponents.get(j).getName());
+			flextableComponentsInProduct.setText(rowNumToInsert, 1,
+					String.valueOf(newListOfAggregatedComponentsAmount.get(j)));
+		}
+
+		RootPanel.get("rightside").add(flextableComponentsInProduct);
+	}
+
+	private void loadAllComponentsOfComponentGroup(ComponentGroup cg, int amount) {
+		// Add All Components To List
+		for (int i = 0; i < cg.getComponentList().size(); i++) {
+			this.allComponentsOfProduct.add(cg.getComponentList().get(i));
+			this.amountComponentsOfProduct.add(cg.getAmountListOfComponent().get(i) * amount);
+		}
+
+		for (int i = 0; i < cg.getComponentgroupList().size(); i++) {
+			loadAllComponentsOfComponentGroup(cg.getComponentgroupList().get(i), amount
+					* cg.getAmountListOfComponentGroup().get(i));
+		}
+
 	}
 
 	private void loadAllProducts() {
@@ -57,38 +185,67 @@ public class MaterialResource extends VerticalPanel {
 			@Override
 			public void onSuccess(ArrayList<Product> result) {
 				for (int i = 0; i < result.size(); i++) {
-					ListBoxOfChoseableProducts.addItem(" - "
-							+ result.get(i).getId() + ":"
+					ListBoxOfChoseableProducts.addItem(" - " + result.get(i).getId() + ":"
 							+ result.get(i).getProductName());
+					allProducts.add(result.get(i));
 				}
 			}
 		});
 
 	}
 
-	// Load
-	public void onLoad() {
-		loadAllProducts();
-		// RootPanel Matching
-		RootPanel.get("rightside").clear();
-		PanelToGenerateProductResources.add(new Label(
-				"Produkt zur Materialbedarfsrechnung auswählen"));
-		PanelToGenerateProductResources.add(ListBoxOfChoseableProducts);
-		PanelToGenerateProductResources.add(new Label("Anzahl eingeben"));
-		PanelToGenerateProductResources.add(AmountOfChosenProducts);
-		PanelToGenerateProductResources.add(buttonToGenerateReport);
-		
-		RootPanel.get("rightside").add(PanelToGenerateProductResources);
-
-		// Buttonclickhandler
-		buttonToGenerateReport.addClickHandler(new ClickHandler() {
+	private void loadComponentsANDComponentGroup() {
+		asyncObj.loadAllComponentGroupsIncludingRelations(new AsyncCallback<ArrayList<ComponentGroup>>() {
 
 			@Override
-			public void onClick(ClickEvent event) {
+			public void onFailure(Throwable caught) {
 				// TODO Auto-generated method stub
 
 			}
+
+			@Override
+			public void onSuccess(ArrayList<ComponentGroup> ComponentGroups) {
+
+				for (ComponentGroup componentgroup : ComponentGroups) {
+					allComponentGroups.add(componentgroup);
+				}
+
+				asyncObj.loadAllComponents(new AsyncCallback<ArrayList<Component>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onSuccess(ArrayList<Component> Components) {
+
+						for (Component component : Components) {
+
+							allComponents.add(component);
+						}
+					}
+				});
+
+			}
+
 		});
+	}
+
+	private int getIdFromProduct(String dropdowntext) {
+		int ProductID = -1;
+
+		// get ID by ListBox text
+		// Splitt " - 569:Bla" into " - 569", "Bla"
+		String[] SplitStepOne = dropdowntext.split(":");
+
+		// Splitt " - 569" into " - ", "569"
+		String[] SplitStepTwo = SplitStepOne[0].split(" ");
+
+		ProductID = Integer.valueOf(SplitStepTwo[1]);
+
+		return ProductID;
 	}
 
 }
--- HdM SMS/src/de/hdm/sms/client/report/StructureStocklist.java
+++ HdM SMS/src/de/hdm/sms/client/report/StructureStocklist.java
@@ -5,30 +5,93 @@ import java.util.ArrayList;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.sms.shared.AService;
 import de.hdm.sms.shared.AServiceAsync;
+import de.hdm.sms.shared.LoginInfo;
 import de.hdm.sms.shared.ReportService;
+import de.hdm.sms.shared.bo.Component;
+import de.hdm.sms.shared.bo.ComponentGroup;
 import de.hdm.sms.shared.bo.Stocklist;
+import de.hdm.sms.shared.bo.User;
 
 public class StructureStocklist extends VerticalPanel {
 
 	private final AServiceAsync asyncObj = GWT.create(AService.class);
-	private final ListBox ListOfShowableStocklists = new ListBox();
-	private final Button buttonToGenerateStructureStocklist = new Button(
-			"Strukturstückliste anzeigen");
-	private final VerticalPanel PanelToAddStructureStocklistData = new VerticalPanel();
+
+	private ArrayList<Component> allComponents = new ArrayList<Component>();
+	private ArrayList<ComponentGroup> allComponentGroups = new ArrayList<ComponentGroup>();
+	private ArrayList<User> allUsers = new ArrayList<User>();
 	private ArrayList<Stocklist> allStocklists = new ArrayList<Stocklist>();
 
-	private void loadStocklist() {
+	// GUI Objects
+	// Select (FIRSTPAGE)
+	private final HorizontalPanel PanelSelectStocklistToEdit = new HorizontalPanel();
+	private final ListBox listboxListOfStocklistsToEdit = new ListBox();
+	private final Button buttonEditStocklist = new Button("Editieren");
 
-		asyncObj.loadAllStocklistsIncludingRelations(new AsyncCallback<ArrayList<Stocklist>>() {
+	DateTimeFormat dF = DateTimeFormat.getFormat("dd.MM.yyyy hh:mm:ss");
+
+	Tree tree = new Tree();
+	TreeItem rootTreeItem = new TreeItem();
+	
+	// Load
+	public void onLoad() {
+		// Load
+		loadStocklist();
+		loadComponentsANDComponentGroup();
+		loadAllUser();
+
+		
+		// Panel: Select StocklistToEdit
+		PanelSelectStocklistToEdit.add(new Label("Wahlen Sie die Stueckliste aus, die Sie editieren moechten:"));
+		PanelSelectStocklistToEdit.add(listboxListOfStocklistsToEdit);
+		PanelSelectStocklistToEdit.add(buttonEditStocklist);
+
+		buttonEditStocklist.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				if (listboxListOfStocklistsToEdit.getItemText(listboxListOfStocklistsToEdit.getSelectedIndex()).equals(
+						"Stueckliste"))
+					Window.alert("Bitte eine Stueckliste auswaehlen");
+				else {
+					// get selected Item
+					String IdOfSelectedItem = getELementTypeIdName(listboxListOfStocklistsToEdit
+							.getItemText(listboxListOfStocklistsToEdit.getSelectedIndex()))[1];
+					for (Stocklist stocklist : allStocklists) {
+
+						if (stocklist.getId() == Integer.valueOf(IdOfSelectedItem)) {
+							LoadStocklistToEdit(stocklist); // load selected
+															// item
+							break;
+						}
+
+					}
+				}
+			}
+		});
+
+		RootPanel.get("rightside").add(PanelSelectStocklistToEdit);
+	}
+
+	private void loadComponentsANDComponentGroup() {
+		asyncObj.loadAllComponentGroupsIncludingRelations(new AsyncCallback<ArrayList<ComponentGroup>>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
@@ -37,19 +100,32 @@ public class StructureStocklist extends VerticalPanel {
 			}
 
 			@Override
-			public void onSuccess(ArrayList<Stocklist> StockLists) {
-				ListOfShowableStocklists.addItem("Stueckliste");
-				for (Stocklist stocklist : StockLists) {
-					ListOfShowableStocklists.addItem(" - " + stocklist.getId()
-							+ ":" + stocklist.getName());
-					allStocklists.add(stocklist);
+			public void onSuccess(ArrayList<ComponentGroup> ComponentGroups) {
+
+				for (ComponentGroup componentgroup : ComponentGroups) {
+					allComponentGroups.add(componentgroup);
 				}
+
+				asyncObj.loadAllComponents(new AsyncCallback<ArrayList<Component>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onSuccess(ArrayList<Component> Components) {
+
+						for (Component component : Components) {
+							allComponents.add(component);
+						}
+					}
+				});
+
 			}
+
 		});
-	}
-
-	public StructureStocklist() {
-
 	}
 
 	// Get the Id by splitting the Listbox value
@@ -65,27 +141,162 @@ public class StructureStocklist extends VerticalPanel {
 		return SplitStepTwo[1];
 	}
 
-	// Load
-	public void onLoad() {
-		loadStocklist();
-		ListOfShowableStocklists.setSize("180px", "35px");
-
-		// RootPanel Matching
+	private void LoadStocklistToEdit(Stocklist stocklist_) {
 		RootPanel.get("rightside").clear();
-		PanelToAddStructureStocklistData.add(new Label(
-				"Bitte eine Strukturstückliste auswählen"));
-		PanelToAddStructureStocklistData.add(ListOfShowableStocklists);
-		PanelToAddStructureStocklistData
-				.add(buttonToGenerateStructureStocklist);
 
-		RootPanel.get("rightside").add(PanelToAddStructureStocklistData);
-		buttonToGenerateStructureStocklist.addClickHandler(new ClickHandler() {
+		HorizontalPanel PanelSLInfo_Name = new HorizontalPanel();
+		PanelSLInfo_Name.add(new Label("Name:"));
+		PanelSLInfo_Name.add(new Label(stocklist_.getName()));
+		RootPanel.get("rightside").add(PanelSLInfo_Name);
+
+		HorizontalPanel PanelSLInfo_CreeationDate = new HorizontalPanel();
+		PanelSLInfo_CreeationDate.add(new Label("Creationdate:"));
+		PanelSLInfo_CreeationDate.add(new Label(dF.format(stocklist_.getCreationDate())));
+		RootPanel.get("rightside").add(PanelSLInfo_CreeationDate);
+
+		HorizontalPanel PanelSLInfo_LastEditor = new HorizontalPanel();
+		PanelSLInfo_LastEditor.add(new Label("Modifier:"));
+		String lastModifier = "Unknown";
+		for (User user : allUsers) {
+			if (user.getId() == stocklist_.getModifier()) {
+				lastModifier = user.getFirstName() + " " + user.getLastName();
+				break;
+			}
+		}
+		PanelSLInfo_LastEditor.add(new Label(lastModifier));
+		RootPanel.get("rightside").add(PanelSLInfo_LastEditor);
+
+		HorizontalPanel PanelSLInfo_LastModified = new HorizontalPanel();
+		PanelSLInfo_LastModified.add(new Label("Last Modified:"));
+		PanelSLInfo_LastModified.add(new Label(dF.format(stocklist_.getCreationDate())));
+		RootPanel.get("rightside").add(PanelSLInfo_LastModified);
+
+		// Tree
+		// Add all Components
+		rootTreeItem.setText(stocklist_.getName());
+
+		for (int j = 0; j < stocklist_.getComponentList().size(); j++) {
+
+			TreeItem componentItem = new TreeItem();
+
+			String componentItemText = "BT - ";
+			componentItemText += stocklist_.getAmountListOfComponent().get(j).toString();
+			componentItemText += "x - ";
+			componentItemText += stocklist_.getComponentList().get(j).getName();
+			componentItem.setText(componentItemText);
+			componentItem.setTitle("BT" + stocklist_.getComponentList().get(j).getId());
+			rootTreeItem.addItem(componentItem);
+		}
+
+		// Add all ComponentGroups
+		for (int j = 0; j < stocklist_.getComponentGroupList().size(); j++) {
+
+			TreeItem componenGroupitem = new TreeItem();
+
+			String componentGroupItemText = "BG - ";
+			componentGroupItemText += stocklist_.getAmountListOfComponentGroup().get(j).toString();
+			componentGroupItemText += "x - ";
+			componentGroupItemText += stocklist_.getComponentGroupList().get(j).getComponentGroupName();
+			componenGroupitem.setText(componentGroupItemText);
+			componenGroupitem.setTitle("BG" + stocklist_.getComponentGroupList().get(j).getId());
+			componenGroupitem = fillTreeItem(componenGroupitem, stocklist_.getComponentGroupList().get(j));
+			rootTreeItem.addItem(componenGroupitem);
+		}
+
+		tree.addItem(rootTreeItem);
+
+		HTML treeHtml = new HTML(tree.toString());
+		Window.alert("Hallo");
+		RootPanel.get("rightside").add(treeHtml);
+
+	}
+
+	private TreeItem fillTreeItem(TreeItem treeItem, ComponentGroup cg) {
+
+		for (int i = 0; i < cg.getComponentgroupList().size(); i++) {
+			TreeItem componenGroupItem = new TreeItem();
+
+			String item = "BG - ";
+			item += cg.getAmountListOfComponentGroup().get(i).toString();
+			item += "x - ";
+			item += cg.getComponentgroupList().get(i).getComponentGroupName();
+			componenGroupItem.setText(item);
+
+			componenGroupItem = fillTreeItem(componenGroupItem, cg.getComponentgroupList().get(i));
+
+			treeItem.addItem(componenGroupItem);
+		}
+
+		for (int i = 0; i < cg.getComponentList().size(); i++) {
+			TreeItem componenItem = new TreeItem();
+			String item = "BT - ";
+			item += cg.getAmountListOfComponent().get(i).toString();
+			item += "x - ";
+			item += cg.getComponentList().get(i).getName();
+			componenItem.setText(item);
+			treeItem.addItem(componenItem);
+		}
+
+		return treeItem;
+	}
+
+	private void loadStocklist() {
+
+		asyncObj.loadAllStocklistsIncludingRelations(new AsyncCallback<ArrayList<Stocklist>>() {
 
 			@Override
-			public void onClick(ClickEvent event) {
+			public void onFailure(Throwable caught) {
 				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onSuccess(ArrayList<Stocklist> StockLists) {
+				listboxListOfStocklistsToEdit.addItem("Stueckliste");
+				for (Stocklist stocklist : StockLists) {
+					listboxListOfStocklistsToEdit.addItem(" - " + stocklist.getId() + ":" + stocklist.getName());
+					allStocklists.add(stocklist);
+				}
+			}
+		});
+	}
+
+	private String[] getELementTypeIdName(String DropDownText) {
+
+		String[] Element = new String[3];
+
+		Element[0] = "";
+
+		// get ID by ListBox text
+		// Splitt " - 569:Bla" into " - 569", "Bla"
+		String[] SplitStepOne = DropDownText.split(":");
+
+		// Splitt " - 569" into " - ", "569"
+		String[] SplitStepTwo = SplitStepOne[0].split(" ");
+
+		Element[1] = SplitStepTwo[1];
+
+		Element[2] = DropDownText.substring(SplitStepOne[0].length() + 1);
+		return Element;
+	}
+
+	private void loadAllUser() {
+		asyncObj.loadAllUsers(new AsyncCallback<ArrayList<User>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onSuccess(ArrayList<User> result) {
+				for (int i = 0; i < result.size(); i++) {
+					allUsers.add(result.get(i));
+				}
 
 			}
 		});
 	}
+
 }
